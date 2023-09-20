45b274b92df78225e85a411179c8f12235738d37
dam2a <abaena@elpuig.xeill.net>
2023-09-20 17:48:47 +0200

    Mejor

---
+++ src/CharsFileReader.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class CharsFileReader {
+
+    public static void main(String[] args) {
+
+        try {
+
+            BufferedReader inputStream = new BufferedReader(new FileReader(new File("alphabeta5465.txt")));
+            int c = inputStream.read();
+
+            while (c != -1) {
+                System.out.println((char) c);
+                c = inputStream.read();
+            }
+
+            inputStream.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
