4a399cfcc40deccbb3729f20307cec00772f58d0
dam2a <abaena@elpuig.xeill.net>
2023-09-20 16:16:37 +0200

    Mejor

--- src/CharsFileToScreen1.java
+++ src/CharsFileToScreen1.java
@@ -3,10 +3,13 @@ import java.io.*;
 public class CharsFileToScreen1 {
     public static void main(String[] args) {
         String caracteres;
+        char carcacter;
         try {
             BufferedReader output = new BufferedReader(new FileReader("alphabeta.txt"));
             while((caracteres = output.readLine()) != null){
-                System.out.println(caracteres);
+                carcacter = caracteres.charAt(0);
+                carcacter = (char) (carcacter-32);
+                System.out.println(caracteres + "," + carcacter);
             }
             output.close();
         } catch (FileNotFoundException e) {
