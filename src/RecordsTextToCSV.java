f6fe38f31e7c8c86e51bfc294315f609bcbf6ece
dam2a <abaena@elpuig.xeill.net>
2023-09-27 16:49:39 +0200

    Mejor

--- src/RecordsTextToCSV.java
+++ src/RecordsTextToCSV.java
@@ -1,34 +1,67 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
+
 public class RecordsTextToCSV {
+    static ArrayList<String> noms, cognoms, edat;
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        String fichero,linea;
-        String[] personas = new String[3];
+        noms = new ArrayList<String>();
+        cognoms = new ArrayList<String>();
+        edat = new ArrayList<String>();
 
-        System.out.println("Escribe el nombre del fichero que contiene personas");
-        fichero = entrada.nextLine();
-
+        llegeixFitxer(new File("personas.txt"));
+        mostraPerPantalla();
+        escriuSortidaCSV(new File("personas.csv"));
+    }
+    public static void llegeixFitxer(File f){
         try {
-            BufferedReader output = new BufferedReader(new FileReader(fichero));
-            File file = new File("persona.csv");
-            BufferedWriter input = new BufferedWriter(new FileWriter(file));
-            input.write("Nombre,Apellido,edad");
-            input.newLine();
+            BufferedReader inputStream = new BufferedReader(new FileReader(f));
 
-            while((linea = output.readLine()) != null){
-                for (int i = 0; i < 3; i++){
-                    personas[i] = linea;
-                }
-                input.write(personas[0]+","+personas[1]+","+personas[2]);
-                input.newLine();
+            String line = "";
+
+            while ((line = inputStream.readLine()) != null ) {
+                noms.add(line);
+                line = inputStream.readLine();
+                cognoms.add(line);
+                line = inputStream.readLine();
+                edat.add(line);
+                line = inputStream.readLine();
             }
-            output.close();
-            input.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+            inputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+    public static void mostraPerPantalla() {
+        System.out.println("nom, cognoms, edat");
+
+        for (int i = 0; i < noms.size() ; i++) {
+            System.out.print(noms.get(i) + "," + cognoms.get(i) +"," + edat.get(i));
+            System.out.println();
+        }
+    }
+    public static void escriuSortidaCSV(File f) {
+        try {
+            BufferedWriter outputStream = new BufferedWriter(new FileWriter(f, false));
+
+            outputStream.write("nom, cognoms, edat");
+            outputStream.newLine();
+
+            for (int i = 0; i < noms.size() ; i++) {
+                outputStream.write(noms.get(i));
+                outputStream.write(",");
+                outputStream.write(cognoms.get(i));
+                outputStream.write(",");
+                outputStream.write(edat.get(i));
+                outputStream.newLine();
+            }
+
+            outputStream.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
